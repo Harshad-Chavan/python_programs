cf523505ee5f62485c64a15577a8bbfdab4258d5
Harshad-Chavan <harshadchavan141@gmail.com>
2019-12-24 21:55:20 +0530

    Update mat_pattern.py

--- mat_pattern.py
+++ mat_pattern.py
@@ -1,16 +1,10 @@
 if __name__ == '__main__':
     N, M = map(int, input().split())
-    def front_end_strings(remaning_spaces):
-        front_string = "-" * (int(remaning_spaces / 2))
-        end_string = "-" * (int(remaning_spaces / 2))
-        return front_string, end_string
     def welcome_str(M):
-        //final_string = front_end_strings(M - 7)[0] + "welcome" + front_end_strings(M - 7)[1]
-        print 'WELCOME'.center(M,'-')
+        final_string = 'WELCOME'.center(M,'-')
         return final_string
     def pattern_str(M, num):
-        no_of_patterns = num
-        final_string = front_end_strings(M - (no_of_patterns * 3))[0] + ".|." * int(num) + front_end_strings(M - (no_of_patterns * 3))[1]
+        final_string = (".|." * num).center(M,'-')
         return final_string
     pattern_counter = 1
     for lines in range(0, N):
